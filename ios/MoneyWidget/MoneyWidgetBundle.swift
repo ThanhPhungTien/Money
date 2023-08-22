08673e939adb5f9aa363c22e33fcbb5b767ed542
Phùng Tiến Thành - Internal <thanh.pt1@kiotviet.com>
2023-08-22 16:48:31 +0700

    update base

---
+++ ios/MoneyWidget/MoneyWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyWidgetBundle.swift
+//  MoneyWidget
+//
+//  Created by thanh.pt1 on 07/08/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MoneyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MoneyWidget()
+        MoneyWidgetLiveActivity()
+    }
+}
